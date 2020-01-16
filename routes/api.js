82a3d45ecf3d9d79a8cad61323cb35acb509ed4a
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2020-01-16 14:35:08 -0600

    Moved folders from VS Code to folder.

---
+++ routes/api.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// create a put route for /api/workouts/:id that updates the workout
+
+router.put("/api/workouts/:id", ({body, params}, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: { exercise: body }
+    },
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// create a get route for /api/workouts that gets all the workouts
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// create a get route for /api/workouts/range that gets all the workouts by a range
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(3)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// create a delete route for /api/workouts that finds a workout and deletes from the database.
+
+router.delete("/api/workouts", ({body}, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+module.exports = router;
